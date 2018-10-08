bd0500f01c580293281ca30e41f7e1003fc393d3
José Henrique da Silva guimarães <42553922+JHenriqueSG@users.noreply.github.com>
2018-10-08 20:17:05 -0300

    Add files via upload

---
+++ Exercico2-2/Exercico2-2/Cliente.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Exercico2_2
+{
+    public class Cliente
+    {
+        private int Saldo;
+        private int Conta;
+        private int Cobranças;
+        private int Credito;
+        private int LimiteDeCredito;
+
+        public Cliente(int Saldo, int Conta, int Cobrança, int Credito, int LimiteDeCredito)
+        {
+            this.Saldo = Saldo;
+            this.Conta = Conta;
+            this.Cobranças = Cobrança;
+            this.Credito = Credito;
+            this.LimiteDeCredito = LimiteDeCredito;
+        }
+
+        public int CalcularNovoSaldo()
+        {
+            return this.Saldo + this.Cobranças - this.Credito;
+        }
+
+        public void AtualizarSaldo()
+        {
+            this.Saldo = CalcularNovoSaldo();
+        }
+
+        public bool ultrapassarLimite()
+        {
+            return this.Saldo > LimiteDeCredito;
+        }
+
+
+    }
+}
---
+++ Exercico2-2/Exercico2-2/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Exercico2_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Cliente A = new Cliente(2000, 200, 300, 400, 2000);
+
+            Console.WriteLine(A.CalcularNovoSaldo());
+
+            A.AtualizarSaldo();
+
+            if(A.ultrapassarLimite())
+            {
+                Console.WriteLine("Limite de credito excedido!");
+
+            }
+
+
+        }
+    }
+}
